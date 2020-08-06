46c1f5828c08dd2d3ee3c53cd382dfd699a132c1
ClappeB <bruno.clappe@viacesi.fr>
2020-08-06 10:41:47 +0200

    Amelioration du script accueil

--- public/assets/js/general/first_visit.js
+++ public/assets/js/general/first_visit.js
@@ -5,13 +5,23 @@
     var FV_Container = document.createElement("div");
     var FV_Row = document.createElement("div");
     var FV_Text_Div = document.createElement("div");
+    var FV_Text = document.createElement("div");
     var FV_Flag_Div = document.createElement("div");
     var FV_Flags = [];
+    var FV_Decorating_Div = [];
 
     initStyle();
     addFlags();
 
+    for(let i=0; i<2; i++){
+        let divDecoratingElement = document.createElement('div');
+        divDecoratingElement.classList.add("FV-animated-div", "no-display");
+        FV_Decorating_Div.push(divDecoratingElement);
+    }
+
     FV_Flags.forEach(flag => FV_Flag_Div.append(flag));
+    FV_Decorating_Div.forEach(div => FV_Text_Div.append(div));
+    FV_Text_Div.append(FV_Text);
     FV_Container.append(FV_Text_Div);
     FV_Container.append(FV_Flag_Div);
     FV_Row.append(FV_Container);
@@ -27,14 +37,14 @@ function addFlags(){
     for(let i=0; i<flagNames.length; i++){
         let flag = document.createElement("img");
         flag.src=pathToFlagFile+flagNames[i]+flagIconName;
-        flag.classList.add('FV-flag', 'my-2');
-        flag.id=flagNames[i];
-        flag.tabIndex=0;
-        flag.onmouseenter=function(){flagFocus(this)};
+        flag.classList.add('FV-flag-img');
 
-        let linkFlag = document.createElement("a");
+        let linkFlag = document.createElement("button");
         linkFlag.onclick=function(){languageChoose(flagNames[i])};
-        linkFlag.classList.add("link-flag", "m-md-3", "p-md-3", "m-2", "p-2");
+        linkFlag.classList.add("FV-flag", "p-0", "mx-lg-5", "mx-md-3", "m-2");
+        linkFlag.id=flagNames[i];
+        //flag.tabIndex=0;
+        linkFlag.onmouseenter=function(){flagFocus(this)};
         linkFlag.append(flag);
 
         FV_Flags.push(linkFlag);
@@ -68,21 +78,24 @@ function initStyle(){
 
     FV_Container.classList.add("container-fluid", "p-0");
 
-    FV_Text_Div.classList.add('col-12');
-    FV_Text_Div.style.fontFamily="Segoe UI";
-    FV_Text_Div.style.color="white";
+    FV_Text_Div.classList.add('col-12', "FV-animated-row", "p-0", "m-0");
+
+    FV_Text.classList.add('col-12', 'pb-md-4', 'pb-3', 'pt-md-0', 'pb-3');
+    FV_Text.style.fontFamily="Segoe UI";
+    FV_Text.style.color="white";
 
     FV_Flag_Div.classList.add('col-12', 'm-0', 'p-0', 'no-display');
 }
 
 function changeDisplayedText(text){
-    FV_Text_Div.textContent=text;
+    FV_Text.textContent=text;
 }
 
 function welcome(){
     clearFVDiv();
     changeDisplayedText(translation(defaultLanguage, "welcome")+' !');
-    FV_Text_Div.classList.add('welcome-message');
+    FV_Text.classList.add('welcome-message');
+    FV_Decorating_Div.forEach(div => div.classList.remove("no-display"));
 }
 
 function blackScreen(){
@@ -96,18 +109,22 @@ function choosingLanguage(){
 }
 
 function rearrangeStyleForFlagDisplay() {
-    FV_Text_Div.classList.remove('welcome-message');
-    FV_Text_Div.classList.add("language-message", "mb-4");
+    FV_Text.classList.remove('welcome-message');
+    FV_Text.classList.add("language-message");
 
     FV_Flag_Div.classList.remove('no-display');
     FV_Flag_Div.classList.add("language-message", "mt-2");
 
-    let FV_DefaultFocusedFlag = document.getElementById(defaultLanguage)
+    focusDefaultLanguageFlag();
+}
+
+function focusDefaultLanguageFlag() {
+    let FV_DefaultFocusedFlag = document.getElementById(defaultLanguage);
     if(FV_DefaultFocusedFlag){flagFocus(FV_DefaultFocusedFlag);}
 }
 
 function clearFVDiv(){
-    FV_Text_Div.textContent=null;
+    FV_Text.textContent=null;
 }
 
 function translation(language, message){
